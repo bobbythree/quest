e30067401132782e7a418193b7633ba23d44bc8f
bobbythree <mynameisbobbylewis@hotmail.com>
2024-08-17 23:40:32 -0500

    continued WIP parser and commands

--- game-commands/verbs.js
+++ game-commands/verbs.js
@@ -1,1 +1,2 @@
 export const verbs = ['look', 'look at', 'talk', 'talk to', 'climb', 'get', 'use', 'give', 'drop', 'open', 'close', 'search', 'give', 'push', 'pull', 'throw'];
+
--- parser.js
+++ parser.js
@@ -2,11 +2,14 @@ import { verbs } from './game-commands/verbs.js'
 import { nouns } from './game-commands/nouns.js'
 import { prepositions } from './game-commands/prepositions.js'
 
+
+
 export function parseText(str) {
   const textInput = str.toLowerCase();
   const tokens = textInput.split(' ');
   const parsedArr = [];
   
+  //loop over text input anf identify verbs, nouns, prepositions
   for (let i = 0; i < tokens.length; i++) {
     if (verbs.includes(tokens[i])) {
       parsedArr.push(tokens[i]);
@@ -17,7 +20,43 @@ export function parseText(str) {
     if (prepositions.includes(tokens[i])) {
       parsedArr.push(tokens[i]);
     }
-  }  
-  console.log(parsedArr);
+  }
+
+  //get rid of dupes
+  let noDupesArr = [];
+  parsedArr.forEach((e) => {
+    if (!noDupesArr.includes(e)) {
+      noDupesArr.push(e)
+    }
+  });
+  createCommands(noDupesArr);
+}
+
+function createCommands(input) { 
+  if (input[0] !== 'throw' && input[0] !== 'give' && input[0] !== 'use') {
+    if (verbs.includes(input[0]) && !input[1]) {
+      console.log(`${input[0]}...?`);        
+    } else if (verbs.includes(input[0]) && nouns.includes(input[1])) {
+      runCommand();    
+    } else (console.log('error'));   
+      
+  } else {
+    if (prepositions.includes(input[2]) && nouns.includes(input[3])) {
+      console.log(`you ${input[0]} the ${input[1]} ${input[2]} the ${input[3]}`);    
+    } else if (!input[1]) {
+      console.log(`${input[0]}...?`);
+    } else if (!input[2]) {
+      console.log(`${input[0]} ${input[1]}....?`);      
+    }
+  }
+  
   
 }
+
+function runCommand() {
+  console.log('success');
+  
+}
+console.log(verbs.throw);
+
+
